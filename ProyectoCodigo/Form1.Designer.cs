f325095291623578d58f4283e12ebdef7dcb793b
jose chambi <jose.chambi@ingrammicro.com>
2023-04-15 16:50:50 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoCodigo/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace ProyectoCodigo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnver = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Titulo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Codigo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tipo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(20, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // btnver
+            // 
+            this.btnver.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnver.Location = new System.Drawing.Point(20, 178);
+            this.btnver.Name = "btnver";
+            this.btnver.Size = new System.Drawing.Size(300, 23);
+            this.btnver.TabIndex = 6;
+            this.btnver.Text = "Generar Codigo";
+            this.btnver.UseVisualStyleBackColor = true;
+            this.btnver.Click += new System.EventHandler(this.btnver_Click);
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnguardar.Location = new System.Drawing.Point(20, 347);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(300, 23);
+            this.btnguardar.TabIndex = 7;
+            this.btnguardar.Text = "Guardar Codigo";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 210);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 125);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 382);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.btnver);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnver;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProyectoCodigo/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//AÑADIR LA REFERENCIA BARCODELIB
+using BarcodeLib;
+
+namespace ProyectoCodigo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //1.- CREAR UNA CLASE PARA RELLENAR LOS ITEMS DEL COMBO BOX
+        public class OpcionCombo {
+            public int Valor { get; set; }
+            public string Texto { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //2.- CARGAR EL COMBO CON LOS TIPO DE BARCODE
+
+            int indice = 0;
+            foreach (var nombre in Enum.GetNames(typeof(BarcodeLib.TYPE))) 
+            {
+                comboBox1.Items.Add(new OpcionCombo() { Valor = indice, Texto = nombre });
+                indice++;
+            }
+
+            comboBox1.DisplayMember = "Texto";
+            comboBox1.ValueMember = "Valor";
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void btnver_Click(object sender, EventArgs e)
+        {
+            Image imagenCodigo;
+
+
+            int indice = (comboBox1.SelectedItem as OpcionCombo).Valor;
+            BarcodeLib.TYPE tipoCodigo = (BarcodeLib.TYPE)indice;
+
+
+            Barcode codigo = new Barcode();
+            codigo.IncludeLabel = true;
+            codigo.LabelPosition = LabelPositions.BOTTOMCENTER;
+
+            //AQUI PASALE EL TEXTO DEL TXT  "CODIGO"
+            imagenCodigo = codigo.Encode(tipoCodigo, textBox2.Text.Trim(), Color.Black, Color.White, 300, 100);
+
+
+
+
+            //EXTRA Y AL ULTIMO
+            Bitmap imagenTitulo = convertirTextoImagen(textBox1.Text.Trim(),300,Color.White);
+
+            int alto_imagen_nuevo = imagenCodigo.Height + imagenTitulo.Height;
+
+            Bitmap imagenNueva = new Bitmap(300, alto_imagen_nuevo);
+            Graphics dibujar = Graphics.FromImage(imagenNueva);
+
+            dibujar.DrawImage(imagenTitulo, new Point(0, 0));
+            dibujar.DrawImage(imagenCodigo, new Point(0, imagenTitulo.Height));
+
+
+           
+            //pictureBox1.BackgroundImage = imagenCodigo;
+            pictureBox1.BackgroundImage = imagenNueva;
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            Image imagen_codigo = pictureBox1.BackgroundImage.Clone() as Image;
+
+            SaveFileDialog ventana_dialogo = new SaveFileDialog();
+            ventana_dialogo.FileName = string.Format("{0}.png", textBox2.Text.Trim());
+            ventana_dialogo.Filter = "Imagen|*.png";
+
+            if (ventana_dialogo.ShowDialog() == DialogResult.OK)
+            {
+                imagen_codigo.Save(ventana_dialogo.FileName, ImageFormat.Png);
+                MessageBox.Show("Codigo generado", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+        }
+
+
+        public static Bitmap convertirTextoImagen(string texto,int ancho, Color color)
+        {
+            //creamos el objeto imagen Bitmap
+            Bitmap objBitmap = new Bitmap(1, 1);
+            int Width = 0;
+            int Height = 0;
+            //formateamos la fuente (tipo de letra, tamaño)
+            System.Drawing.Font objFont = new System.Drawing.Font("Arial", 16, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+
+            //creamos un objeto Graphics a partir del Bitmap
+            Graphics objGraphics = Graphics.FromImage(objBitmap);
+
+            //establecemos el tamaño según la longitud del texto
+            Width = ancho;
+            Height = (int)objGraphics.MeasureString(texto, objFont).Height + 5;
+            objBitmap = new Bitmap(objBitmap, new Size(Width, Height));
+
+            objGraphics = Graphics.FromImage(objBitmap);
+
+            objGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            objGraphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            objGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+            objGraphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            
+            StringFormat drawFormat = new StringFormat();
+            objGraphics.Clear(color);
+
+            drawFormat.Alignment = StringAlignment.Center;
+            objGraphics.DrawString(texto, objFont, new SolidBrush(Color.Black), new RectangleF(0, (objBitmap.Height / 2) - (objBitmap.Height - 10), objBitmap.Width, objBitmap.Height), drawFormat);
+            objGraphics.Flush();
+            
+
+            return objBitmap;
+        }
+
+
+
+    }
+}
